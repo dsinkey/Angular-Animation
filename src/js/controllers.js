dfd9c9914d97b64900fe8184be39afab02e574f0
Dustin <dustinsinkey1@gmail.com>
2016-01-17 18:07:33 -0800

    Installed Modernizr to detect fallback to animate.css

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -8,11 +8,26 @@ angular.module('myApp.controllers', ['myApp.filters'])
         ];
 
         $scope.inputText = "";
+
         $scope.addItem = function () {
-            $scope.items.push({
-                text: $scope.inputText,
-                completed: false
-            });
-            $scope.inputText = ""
+            var arrTodos = $scope.inputText.split('');
+
+            for (var i = 0; i < arrTodos.length; i++){
+                $scope.items.push({
+                    text: $scope.inputText,
+                    completed: false
+                });
+            }
+                $scope.inputText = "";
         };
-}]);
+}])
+    .animation('.item', function(){
+        return {
+            enter: function(element, done){
+                element.css({'opacity': 1, 'margin-left': 0}, 500, done);
+            },
+            leave: function(element, className, done){
+                element.animate({'opacity': 0, 'margin-left': -230}, 500, done);
+            }
+        }
+    });
